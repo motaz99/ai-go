b1d97912ad2ccf25a34669b1bb476c9d90fb0ad0
Hawzhen <macbookshop@Hawzhens-MacBook-Pro.local>
2025-11-20 12:22:48 +0300

    Making the about and the contact pages

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 // we will add our JavaScript in this file like in the classes
+const flipCards = document.querySelectorAll('.flip-card');
+
+flipCards.forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    const inner = card.querySelector('.flip-card-inner');
+    inner.classList.add('flipped');
+  });
+
+  card.addEventListener('mouseleave', () => {
+    const inner = card.querySelector('.flip-card-inner');
+    inner.classList.remove('flipped');
+  });
+});
